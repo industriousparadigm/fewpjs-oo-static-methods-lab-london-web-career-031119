03b379c82de8c6e50d48c0076f416bc20057adad
mrsalvini <dsgmcosta@gmail.com>
2019-04-26 09:52:49 +0100

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    // removes any non alpha-numeric characters except dash and single quotes (apostrophes)
+    return str.replace(/[^A-Za-z0-9-'\s]+/g, '')
+  }
+
+  static titleize(str) {
+    const badWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "and", "from"]
+    let newStr = [this.capitalize(str.split(" ")[0])]
+
+    str.split(" ").slice(1).forEach(word => {
+      !badWords.includes(word) ? newStr.push(this.capitalize(word)) : newStr.push(word)
+    });
+
+    return newStr.join(" ")
+  }
+
 }
